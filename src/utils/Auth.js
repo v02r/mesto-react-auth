5bb893254540bccca3224f8dbda11fdcad70dd50
v02r <vmprio@yandex.ru>
2023-11-26 11:08:34 +0300

    p12v1

---
+++ src/utils/Auth.js
@@ -0,0 +1,67 @@
+const BASE_URL = "https://auth.nomoreparties.co";
+
+const headers = {
+    Accept: "application/json",
+    "Content-Type": "application/json",
+};
+
+class Auth {
+    constructor({baseUrl, headers}) {
+        this._baseUrl = baseUrl;
+        this._headers = headers;
+    }
+
+    register = ({email, password}) => {
+        return fetch(`${this._baseUrl}/signup`, {
+            method: "POST",
+            headers: this._headers,
+            body: JSON.stringify({email, password}),
+        })
+            .then((res) => {
+                if (res.ok) {
+                    return res.json();
+                }
+                return false;
+            })
+            .catch((err) => {
+                console.log(`Ошибка: ${err}`);
+            });
+    }
+
+    login = ({email, password}) => {
+        return fetch(`${this._baseUrl}/signin`, {
+            method: "POST",
+            headers: this._headers,
+            body: JSON.stringify({email, password}),
+        }).then((res) => {
+            if (res.ok) {
+                return res.json();
+            }
+            return false;
+        }).catch((err) => {
+            console.log(`Ошибка: ${err}`);
+        });
+
+    };
+
+    getUserInfo = (jwt) => {
+        return fetch(`${this._baseUrl}/users/me`, {
+            method: "GET",
+            headers: {
+                ...this._headers,
+                Authorization: `Bearer ${jwt}`,
+            },
+        })
+            .then((res) => {
+                if (res.ok) {
+                    return res.json();
+                }
+                return false;
+            }).catch((err) => {
+                console.log(`Ошибка: ${err}`);
+            });
+    };
+
+}
+
+export default new Auth({baseUrl: BASE_URL, headers});
